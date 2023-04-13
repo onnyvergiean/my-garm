b661cb4ef178b934de00d1a9ec9743304f271271
Onny Vergiean Saputra <onny.s@students.amikom.ac.id>
2023-04-13 14:59:47 +0700

    fix typo

--- models/gormModel.go
+++ models/gormModel.go
@@ -6,6 +6,6 @@ import "time"
 
 type GormModel struct {
 	ID 	  uint `gorm:"primary_key"`
-	CreatedAt *time.Time `json:"created_at",omitempty`
-	UpdatedAt *time.Time `json:"updated_at",omitempty`
+	CreatedAt *time.Time `json:"created_at,omitempty"`
+	UpdatedAt *time.Time `json:"updated_at,omitempty"`
 }
