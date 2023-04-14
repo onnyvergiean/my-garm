9c88c87b75422a8f76894e44178ae85f1da1d8ef
Onny Vergiean Saputra <onny.s@students.amikom.ac.id>
2023-04-14 13:49:02 +0700

    remove json -

--- models/photo.go
+++ models/photo.go
@@ -11,8 +11,8 @@ type Photo struct {
 	Title string `gorm:"not null" json:"title" form:"title" valid:"required~Title is required"`
 	Caption string `gorm:"not null" json:"caption" form:"caption"`
 	PhotoURL string `gorm:"not null" json:"photo_url" form:"photo_url" valid:"required~Photo URL is required"`
-	UserID uint `json:"-"`
-	User *User  `json:"-"`
+	UserID uint 
+	User *User  
 }
 
 func (p *Photo) BeforeCreate(tx *gorm.DB) (err error) {
