9da8f08af34f2fec4f290273264af154a4e2a0be
Onny Vergiean Saputra <onny.s@students.amikom.ac.id>
2023-04-15 10:26:10 +0700

    rm json -

--- models/user.go
+++ models/user.go
@@ -15,7 +15,7 @@ type User struct {
 	GormModel `json:"-"`
 	Username string `gorm:"not null;uniqueIndex" json:"username" form:"username" valid:"required~Username is required"`
 	Email    string `gorm:"not null;uniqueIndex" json:"email" form:"email" valid:"required~Email is required,email~Email is invalid"`
-	Password string `gorm:"not null" json:"-" form:"password" valid:"required~Password is required,minstringlength(6)~Password must be at least 6 characters"`
+	Password string `gorm:"not null" json:"password" form:"password" valid:"required~Password is required,minstringlength(6)~Password must be at least 6 characters"`
 	Age 	int    `gorm:"not null" json:"age" form:"age" valid:"required~Age is required,"`
 	Photos       []Photo       `gorm:"constraint:OnUpdate:CASCADE,OnDelete:CASCADE;" json:"-"`
 	Comments     []Comment     `gorm:"constraint:OnUpdate:CASCADE,OnDelete:CASCADE;" json:"-"`
